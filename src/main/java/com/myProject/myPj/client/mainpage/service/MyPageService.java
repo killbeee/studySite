71d6c9c7cd54857fdce1a1e7d6d68a07b7b10cd0
yrl <yrl@dainls.com>
2024-02-26 10:48:43 +0900

    없을떄도 처리2

--- src/main/java/com/myProject/myPj/client/mainpage/service/MyPageService.java
+++ src/main/java/com/myProject/myPj/client/mainpage/service/MyPageService.java
@@ -1,6 +1,5 @@
 package com.myProject.myPj.client.mainpage.service;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,47 +16,50 @@ import com.myProject.myPj.vo.PagingVo;
 @Service
 public class MyPageService {
 
-    @Autowired
-    private MyPageMapper myPageMapper;
-    @Autowired
-    private CommonService commonService;
+	@Autowired
+	private MyPageMapper myPageMapper;
+	@Autowired
+	private CommonService commonService;
 
-    public List<Map<String,Object>> getShareTodayPost(HttpServletRequest req, Map<String, Object> paramMap) {
-    	PagingVo pagingVo = new PagingVo();
-    	//현재 페이지  num
-    	pagingVo.setCurrentPageNo(Integer.parseInt((String)paramMap.get("currentPageNo")));
-    	//한페이지에 보여질 게시물 갯수
-    	if(commonService.isDevice(req).equals("MOBI")) {
-    		pagingVo.setRecordCountPerPage(2);
-    	}else {
-    		pagingVo.setRecordCountPerPage(6);
-    	}
-    	
-    	//페이지 리스트에 한번에 보일 페이지 갯수
-    	pagingVo.setPageSize(5);
-    	paramMap.put("firstIndex", pagingVo.getFirstRecordIndex());
-    	paramMap.put("lastIndex", pagingVo.getLastRecordIndex());
-    	
-    	List<Map<String,Object>> paramList =  myPageMapper.getShareTodayPost(paramMap);
-    	long totalCnt = (long)paramList.get(0).get("MAX_COUNT");
-    	pagingVo.setTotalRecordCount((int)totalCnt);
-    	Map<String,Object> pagingMap = new HashMap<>();
-    	pagingMap.put("pagingVo", pagingVo);
-    	
+	public List<Map<String, Object>> getShareTodayPost(HttpServletRequest req, Map<String, Object> paramMap) {
+		PagingVo pagingVo = new PagingVo();
+		// 현재 페이지 num
+		pagingVo.setCurrentPageNo(Integer.parseInt((String) paramMap.get("currentPageNo")));
+		// 한페이지에 보여질 게시물 갯수
+		if (commonService.isDevice(req).equals("MOBI")) {
+			pagingVo.setRecordCountPerPage(2);
+		} else {
+			pagingVo.setRecordCountPerPage(6);
+		}
 
-    	paramList.add(pagingMap);
-        return paramList;
-    }
-    public Map<String,Object> modifyPost(Map<String, Object> paramMap) {
+		// 페이지 리스트에 한번에 보일 페이지 갯수
+		pagingVo.setPageSize(5);
+		paramMap.put("firstIndex", pagingVo.getFirstRecordIndex());
+		paramMap.put("lastIndex", pagingVo.getLastRecordIndex());
 
-    	Map<String,Object> post =  myPageMapper.getPost(paramMap);    	
+		List<Map<String, Object>> paramList = myPageMapper.getShareTodayPost(paramMap);
+		if (paramList.size() > 0) {
+			long totalCnt = (long) paramList.get(0).get("MAX_COUNT");
+			pagingVo.setTotalRecordCount((int) totalCnt);
+			Map<String, Object> pagingMap = new HashMap<>();
+			pagingMap.put("pagingVo", pagingVo);
+			paramList.add(pagingMap);
 
-        return post;
-    }
-    public void deletePost(Map<String, Object> paramMap) {
+		}
+		return paramList;
+	}
 
-    	myPageMapper.deletePost(paramMap);    	
-    }
+	public Map<String, Object> modifyPost(Map<String, Object> paramMap) {
+
+		Map<String, Object> post = myPageMapper.getPost(paramMap);
+
+		return post;
+	}
+
+	public void deletePost(Map<String, Object> paramMap) {
+
+		myPageMapper.deletePost(paramMap);
+	}
 
 	public boolean insertPostInfo(Map<String, Object> paramMap) {
 		boolean yes = true;
@@ -65,12 +67,14 @@ public class MyPageService {
 
 		return yes;
 	}
+
 	public boolean insertReply(Map<String, Object> paramMap) {
 		boolean yes = true;
 		yes = myPageMapper.insertReply(paramMap);
 
 		return yes;
 	}
+
 	public boolean delReply(Map<String, Object> paramMap) {
 		boolean yes = true;
 		yes = myPageMapper.delReply(paramMap);
